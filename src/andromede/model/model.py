9e0dbe0aff75a279a9a56a8a296613562d6d69a4
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2024-02-09 14:34:58 +0100

    Add license headers to python files
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- src/andromede/model/model.py
+++ src/andromede/model/model.py
@@ -1,3 +1,15 @@
+# Copyright (c) 2024, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+
 """
 The model module defines the data model for user-defined models.
 A model allows to define the behaviour for components, by
