01973fe29bdeee7e07c48da66f25e62a3f526c00
Thomas Bittar <thomas.bittar@rte-france.com>
2024-02-22 17:56:54 +0100

    Improve test description

--- src/andromede/simulation/time_block.py
+++ src/andromede/simulation/time_block.py
@@ -41,7 +41,9 @@ class TimeBlock:
 @dataclass(frozen=True)
 class ResolutionNode:
     id: str
-    blocks: List[TimeBlock]
+    blocks: List[TimeBlock] # Séparer horizon de simu annuel
     children: List["ResolutionNode"] = field(default_factory=list)
     # solution: Dict[TimestepComponentVariableKey, lp.Variable]
     
+class InBetweenMasterDecisionTimeHorizon:
+    blocks : List[TimeBlock]
