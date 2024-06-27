0b10902bcb3c81fc3e98754e74152e8a062aca77
Juliette-Gerbaux <juliette.gerbaux_externe@rte-france.com>
2024-06-27 13:34:31 +0200

    Move some functions from tests to src

---
+++ src/andromede/thermal_heuristic/data.py
@@ -0,0 +1,171 @@
+# Copyright (c) 2024, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+
+import pandas as pd
+import pytest
+import numpy as np
+from math import ceil
+
+from typing import List
+
+from andromede.simulation.optimization import OptimizationProblem
+
+from andromede.simulation import (
+    OutputValues,
+)
+from andromede.study import (
+    ConstantData,
+    TimeScenarioSeriesData,
+)
+
+
+def get_max_unit_for_min_down_time(delta: int, max_units: pd.DataFrame) -> pd.DataFrame:
+    nb_units_max_min_down_time = pd.DataFrame(
+        np.roll(max_units.values, delta), index=max_units.index
+    )
+    end_failures = max_units - pd.DataFrame(
+        np.roll(max_units.values, 1), index=max_units.index
+    )
+    end_failures.where(end_failures > 0, 0, inplace=True)
+    for j in range(delta):
+        nb_units_max_min_down_time += pd.DataFrame(
+            np.roll(end_failures.values, j), index=end_failures.index
+        )
+
+    return nb_units_max_min_down_time
+
+
+def get_max_failures(max_units: pd.DataFrame) -> pd.DataFrame:
+    max_failures = (
+        pd.DataFrame(np.roll(max_units.values, 1), index=max_units.index) - max_units
+    )
+    max_failures.where(max_failures > 0, 0, inplace=True)
+    return max_failures
+
+
+def get_max_unit(
+    pmax: float, units: float, max_generating: pd.DataFrame
+) -> pd.DataFrame:
+    max_units = max_generating / pmax
+    max_units.where(max_units < units, units, inplace=True)
+    return max_units
+
+
+def get_failures_for_cluster(
+    week: int, scenario: int, cluster: str, number_hours: int, dir_path: str
+) -> pd.DataFrame:
+    input_file = np.loadtxt(
+        "tests/functional/" + dir_path + f"/series_{cluster}.txt",
+        delimiter="\t",
+    )
+
+    failures_data = pd.DataFrame(
+        data=input_file[
+            number_hours * week : number_hours * week + number_hours, scenario
+        ],
+        index=[i for i in range(number_hours)],
+        columns=[0],
+    )
+
+    return failures_data
+
+
+def check_output_values(
+    problem: OptimizationProblem, mode: str, week: int, scenario: int, dir_path: str
+) -> None:
+    output = OutputValues(problem)
+
+    expected_output_clusters_file = open(
+        "tests/functional/"
+        + dir_path
+        + "/"
+        + mode
+        + "/"
+        + str(scenario)
+        + "/details-hourly.txt",
+        "r",
+    )
+    expected_output_clusters = expected_output_clusters_file.readlines()
+
+    expected_output_general_file = open(
+        "tests/functional/"
+        + dir_path
+        + "/"
+        + mode
+        + "/"
+        + str(scenario)
+        + "/values-hourly.txt",
+        "r",
+    )
+    expected_output_general = expected_output_general_file.readlines()
+
+    assert output.component("G1").var("generation").value == [
+        [
+            pytest.approx(float(line.strip().split("\t")[4]))
+            for line in expected_output_clusters[168 * week + 7 : 168 * week + 7 + 168]
+        ]
+    ]
+    if mode != "fast":
+        assert output.component("G1").var("nb_on").value == [
+            [
+                pytest.approx(float(line.strip().split("\t")[12]))
+                for line in expected_output_clusters[
+                    168 * week + 7 : 168 * week + 7 + 168
+                ]
+            ]
+        ]
+
+    assert output.component("G2").var("generation").value == [
+        [
+            pytest.approx(float(line.strip().split("\t")[5]))
+            for line in expected_output_clusters[168 * week + 7 : 168 * week + 7 + 168]
+        ]
+    ]
+    if mode != "fast":
+        assert output.component("G2").var("nb_on").value == [
+            [
+                pytest.approx(float(line.strip().split("\t")[13]))
+                for line in expected_output_clusters[
+                    168 * week + 7 : 168 * week + 7 + 168
+                ]
+            ]
+        ]
+
+    assert output.component("G3").var("generation").value == [
+        [
+            pytest.approx(float(line.strip().split("\t")[6]))
+            for line in expected_output_clusters[168 * week + 7 : 168 * week + 7 + 168]
+        ]
+    ]
+    if mode != "fast":
+        assert output.component("G3").var("nb_on").value == [
+            [
+                pytest.approx(float(line.strip().split("\t")[14]))
+                for line in expected_output_clusters[
+                    168 * week + 7 : 168 * week + 7 + 168
+                ]
+            ]
+        ]
+
+    assert output.component("S").var("spillage").value == [
+        [
+            pytest.approx(float(line.strip().split("\t")[20 if mode == "milp" else 21]))
+            for line in expected_output_general[168 * week + 7 : 168 * week + 7 + 168]
+        ]
+    ]
+
+    assert output.component("U").var("unsupplied_energy").value == [
+        [
+            pytest.approx(float(line.strip().split("\t")[19 if mode == "milp" else 20]))
+            for line in expected_output_general[168 * week + 7 : 168 * week + 7 + 168]
+        ]
+    ]
---
+++ src/andromede/thermal_heuristic/model.py
@@ -0,0 +1,273 @@
+# Copyright (c) 2024, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+
+import pandas as pd
+import pytest
+import numpy as np
+from typing import List
+from math import ceil
+import ortools.linear_solver.pywraplp as pywraplp
+
+from andromede.expression import (
+    literal,
+    param,
+    var,
+    PrinterVisitor,
+    visit,
+    ExpressionNode,
+)
+from andromede.expression.indexing_structure import IndexingStructure
+from andromede.libs.standard import (
+    DEMAND_MODEL,
+    NODE_BALANCE_MODEL,
+    SPILLAGE_MODEL,
+    UNSUPPLIED_ENERGY_MODEL,
+)
+from tests.functional.libs.lib_thermal_heuristic import THERMAL_CLUSTER_MODEL_MILP
+from andromede.model import Model, float_parameter, float_variable, model
+from andromede.model.parameter import float_parameter, int_parameter
+from andromede.model.variable import float_variable, int_variable, ValueType
+from andromede.model.constraint import Constraint
+from andromede.simulation import (
+    BlockBorderManagement,
+    OutputValues,
+    TimeBlock,
+    build_problem,
+)
+from andromede.simulation.optimization import OptimizationProblem
+from andromede.study import (
+    ConstantData,
+    DataBase,
+    Network,
+    Node,
+    PortRef,
+    TimeScenarioSeriesData,
+    TimeSeriesData,
+    TimeIndex,
+    create_component,
+)
+from andromede.study.data import AbstractDataStructure
+
+CONSTANT = IndexingStructure(False, False)
+TIME_AND_SCENARIO_FREE = IndexingStructure(True, True)
+ANTICIPATIVE_TIME_VARYING = IndexingStructure(True, True)
+NON_ANTICIPATIVE_TIME_VARYING = IndexingStructure(True, False)
+CONSTANT_PER_SCENARIO = IndexingStructure(False, True)
+
+def get_thermal_cluster_accurate_model(initial_model: Model) -> Model:
+
+    THERMAL_CLUSTER_MODEL_LP = model(
+        id=initial_model.id,
+        parameters=[p for p in initial_model.parameters.values()],
+        variables=[
+            float_variable(
+                v.name,
+                lower_bound=v.lower_bound,
+                upper_bound=v.upper_bound,
+                structure=v.structure,
+            )
+            for v in initial_model.variables.values()
+        ],
+        ports=[p for p in initial_model.ports.values()],
+        port_fields_definitions=[
+            p for p in initial_model.port_fields_definitions.values()
+        ],
+        constraints=[c for c in initial_model.constraints.values()],
+        objective_operational_contribution=initial_model.objective_operational_contribution,
+    )
+
+    return THERMAL_CLUSTER_MODEL_LP
+
+
+def get_thermal_cluster_fast_model(initial_model: Model) -> Model:
+
+    integer_variables = [
+        v.name
+        for v in initial_model.variables.values()
+        if v.data_type == ValueType.INTEGER
+    ]
+
+    THERMAL_CLUSTER_MODEL_FAST = model(
+        id=initial_model.id,
+        parameters=[p for p in initial_model.parameters.values()],
+        variables=[
+            float_variable(
+                v.name,
+                lower_bound=(
+                    v.lower_bound if v.data_type == ValueType.FLOAT else literal(0)
+                ),
+                upper_bound=(
+                    v.upper_bound if v.data_type == ValueType.FLOAT else literal(0)
+                ),
+                structure=v.structure,
+            )
+            for v in initial_model.variables.values()
+        ],
+        ports=[p for p in initial_model.ports.values()],
+        port_fields_definitions=[
+            p for p in initial_model.port_fields_definitions.values()
+        ],
+        constraints=[
+            c
+            for c in initial_model.constraints.values()
+            if not (variable_in_constraint(c, integer_variables))
+        ],
+        objective_operational_contribution=initial_model.objective_operational_contribution,
+    )
+
+    return THERMAL_CLUSTER_MODEL_FAST
+
+
+def variable_in_constraint(c: Constraint, variables: List[str]) -> bool:
+    res = False
+    if variable_in_expression(c.lower_bound, variables):
+        res = True
+    elif variable_in_expression(c.expression, variables):
+        res = True
+    elif variable_in_expression(c.upper_bound, variables):
+        res = True
+    return res
+
+
+def variable_in_expression(expr: ExpressionNode, variables: List[str]) -> bool:
+    res = False
+    str_expr = visit(expr, PrinterVisitor())
+    for v in variables:
+        if v in str_expr:
+            res = True
+    return res
+
+
+def get_accurate_heuristic_model(initial_model: Model) -> Model:
+
+    generation_variable = ["generation"]
+
+    THERMAL_CLUSTER_MODEL_ACCURATE_HEURISTIC = model(
+        id=initial_model.id,
+        parameters=[p for p in initial_model.parameters.values()],
+        variables=[
+            v
+            for v in initial_model.variables.values()
+            if v.name not in generation_variable
+        ],
+        constraints=[
+            c
+            for c in initial_model.constraints.values()
+            if not (variable_in_constraint(c, generation_variable))
+        ],
+        objective_operational_contribution=(var("nb_on")).sum().expec(),
+    )
+    return THERMAL_CLUSTER_MODEL_ACCURATE_HEURISTIC
+
+
+def get_model_fast_heuristic(Q: int, delta: int) -> Model:
+    BLOCK_MODEL_FAST_HEURISTIC = model(
+        id="BLOCK_FAST",
+        parameters=[
+            float_parameter("n_guide", TIME_AND_SCENARIO_FREE),
+            float_parameter("delta", CONSTANT),
+            float_parameter("n_max", CONSTANT),
+        ]
+        + [
+            int_parameter(f"alpha_{k}_{h}", NON_ANTICIPATIVE_TIME_VARYING)
+            for k in range(Q)
+            for h in range(delta)
+        ]
+        + [
+            int_parameter(f"alpha_ajust_{h}", NON_ANTICIPATIVE_TIME_VARYING)
+            for h in range(delta)
+        ],
+        variables=[
+            float_variable(
+                f"n_block_{k}",
+                lower_bound=literal(0),
+                upper_bound=param("n_max"),
+                structure=CONSTANT_PER_SCENARIO,
+            )
+            for k in range(Q)
+        ]
+        + [
+            float_variable(
+                "n_ajust",
+                lower_bound=literal(0),
+                upper_bound=param("n_max"),
+                structure=CONSTANT_PER_SCENARIO,
+            )
+        ]
+        + [
+            int_variable(
+                f"t_ajust_{h}",
+                lower_bound=literal(0),
+                upper_bound=literal(1),
+                structure=CONSTANT_PER_SCENARIO,
+            )
+            for h in range(delta)
+        ]
+        + [
+            float_variable(
+                "n",
+                lower_bound=literal(0),
+                upper_bound=param("n_max"),
+                structure=TIME_AND_SCENARIO_FREE,
+            )
+        ],
+        constraints=[
+            Constraint(
+                f"Definition of n block {k} for {h}",
+                var(f"n_block_{k}")
+                >= param("n_guide") * param(f"alpha_{k}_{h}")
+                - param("n_max") * (literal(1) - var(f"t_ajust_{h}")),
+            )
+            for k in range(Q)
+            for h in range(delta)
+        ]
+        + [
+            Constraint(
+                f"Definition of n ajust for {h}",
+                var(f"n_ajust")
+                >= param("n_guide") * param(f"alpha_ajust_{h}")
+                - param("n_max") * (literal(1) - var(f"t_ajust_{h}")),
+            )
+            for h in range(delta)
+        ]
+        + [
+            Constraint(
+                f"Definition of n with relation to block {k} for {h}",
+                var(f"n")
+                >= param(f"alpha_{k}_{h}") * var(f"n_block_{k}")
+                - param("n_max") * (literal(1) - var(f"t_ajust_{h}")),
+            )
+            for k in range(Q)
+            for h in range(delta)
+        ]
+        + [
+            Constraint(
+                f"Definition of n with relation to ajust for {h}",
+                var(f"n")
+                >= param(f"alpha_ajust_{h}") * var(f"n_ajust")
+                - param("n_max") * (literal(1) - var(f"t_ajust_{h}")),
+            )
+            for h in range(delta)
+        ]
+        + [
+            Constraint(
+                "Choose one t ajust",
+                literal(0) + sum([var(f"t_ajust_{h}") for h in range(delta)])
+                == literal(1),
+            )
+        ],
+        objective_operational_contribution=(var("n")).sum().expec()
+        + sum(
+            [var(f"t_ajust_{h}") * (h + 1) / 10 / delta for h in range(delta)]
+        ).expec(),  # type:ignore
+    )
+    return BLOCK_MODEL_FAST_HEURISTIC
