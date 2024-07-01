2bd37c346340ab69bfec5c66db7e9cd380f831d2
Juliette-Gerbaux <juliette.gerbaux_externe@rte-france.com>
2024-07-01 14:32:09 +0200

    Fix test

--- src/andromede/hydro_heuristic/data.py
+++ src/andromede/hydro_heuristic/data.py
@@ -33,10 +33,10 @@ class HydroHeuristicData:
         self.folder_name = folder_name
         if horizon == "monthly":
             hours_aggregated_time_steps = [
-                24 * get_number_of_days_in_month(m) for m in timesteps
+                24 * get_number_of_days_in_month(m) for m in range(12)
             ]
         elif horizon == "daily":
-            hours_aggregated_time_steps = [24 for d in timesteps]
+            hours_aggregated_time_steps = [24 for d in range(365)]
         self.hours_aggregated_time_steps = hours_aggregated_time_steps
         self.timesteps = timesteps
         self.capacity = capacity
