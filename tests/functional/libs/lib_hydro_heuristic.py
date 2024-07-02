614f554e3adb38524f4b6f9b3f7f76653a8d96fb
Juliette-Gerbaux <juliette.gerbaux_externe@rte-france.com>
2024-07-02 12:21:41 +0200

    Decompose yearly problem into weekly problems for hydro

--- tests/functional/libs/lib_hydro_heuristic.py
+++ tests/functional/libs/lib_hydro_heuristic.py
@@ -14,8 +14,12 @@ from andromede.expression import literal, param, var
 from andromede.expression.indexing_structure import IndexingStructure
 from andromede.model import float_parameter, float_variable, model
 from andromede.model.constraint import Constraint
+from andromede.model.model import ModelPort, PortFieldDefinition, PortFieldId
 from andromede.model.parameter import float_parameter
+from andromede.model.port import PortField, PortType
 from andromede.model.variable import float_variable
+
+BALANCE_PORT_TYPE = PortType(id="balance", fields=[PortField("flow")])
 
 CONSTANT = IndexingStructure(False, False)
 TIME_AND_SCENARIO_FREE = IndexingStructure(True, True)
@@ -81,5 +85,84 @@ HYDRO_MODEL = model(
             - var("overflow").eval(literal(0))
             + param("inflow").eval(literal(0)),
         ),
+    ],
+    ports=[ModelPort(port_type=BALANCE_PORT_TYPE, port_name="balance_port")],
+    port_fields_definitions=[
+        PortFieldDefinition(
+            port_field=PortFieldId("balance_port", "flow"),
+            definition=var("generating"),
+        )
+    ],
+)
+
+HYDRO_MODEL_WITH_TARGET = model(
+    id="H",
+    parameters=[
+        float_parameter("max_generating", NON_ANTICIPATIVE_TIME_VARYING),
+        float_parameter("min_generating", NON_ANTICIPATIVE_TIME_VARYING),
+        float_parameter("capacity", CONSTANT),
+        float_parameter("initial_level", CONSTANT_PER_SCENARIO),
+        float_parameter("inflow", TIME_AND_SCENARIO_FREE),
+        float_parameter(
+            "max_epsilon", NON_ANTICIPATIVE_TIME_VARYING
+        ),  # not really a parameter, it is just to implement correctly one constraint
+        float_parameter("lower_rule_curve", NON_ANTICIPATIVE_TIME_VARYING),
+        float_parameter("upper_rule_curve", NON_ANTICIPATIVE_TIME_VARYING),
+        float_parameter("overall_target", CONSTANT_PER_SCENARIO),
+    ],
+    variables=[
+        float_variable(
+            "generating",
+            lower_bound=param("min_generating"),
+            upper_bound=param("max_generating"),
+            structure=TIME_AND_SCENARIO_FREE,
+        ),
+        float_variable(
+            "level",
+            lower_bound=literal(0),
+            upper_bound=param("capacity"),
+            structure=TIME_AND_SCENARIO_FREE,
+        ),
+        float_variable(
+            "overflow",
+            lower_bound=literal(0),
+            structure=TIME_AND_SCENARIO_FREE,
+        ),
+        float_variable(
+            "epsilon",
+            lower_bound=-param("max_epsilon"),
+            upper_bound=param("max_epsilon"),
+            structure=TIME_AND_SCENARIO_FREE,
+        ),
+    ],
+    constraints=[
+        Constraint(
+            "Level balance",
+            var("level")
+            == var("level").shift(-1)
+            - var("generating")
+            - var("overflow")
+            + param("inflow")
+            + var("epsilon"),
+        ),
+        Constraint(
+            "Initial level",
+            var("level").eval(literal(0))
+            == param("initial_level")
+            - var("generating").eval(literal(0))
+            - var("overflow").eval(literal(0))
+            + param("inflow").eval(literal(0)),
+        ),
+        Constraint(
+            "Respect generating target",
+            var("generating").sum() + var("overflow").sum() == param("overall_target"),
+        ),
+    ],
+    ports=[ModelPort(port_type=BALANCE_PORT_TYPE, port_name="balance_port")],
+    port_fields_definitions=[
+        PortFieldDefinition(
+            port_field=PortFieldId("balance_port", "flow"),
+            definition=var("generating"),
+        )
     ],
 )
