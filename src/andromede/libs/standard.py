6ade49d3bcc730545239613723ae0b41b2bd1315
Ian Menezes <ian.menezes@rte-france.com>
2024-07-09 09:49:12 +0200

    Add RES and Thermal cluster models to standard

--- src/andromede/libs/standard.py
+++ src/andromede/libs/standard.py
@@ -134,6 +134,23 @@ GENERATOR_MODEL = model(
     .expec(),
 )
 
+"""
+A standard model for renewable sources as wind, solar and run-of-river
+"""
+RES_MODEL = model(
+    id="RENEWABLE_PRODUCTION",
+    parameters=[
+        float_parameter("production"),
+    ],
+    ports=[ModelPort(port_type=BALANCE_PORT_TYPE, port_name="balance_port")],
+    port_fields_definitions=[
+        PortFieldDefinition(
+            port_field=PortFieldId("balance_port", "flow"),
+            definition=param("production"),
+        )
+    ],
+)
+
 GENERATOR_MODEL_WITH_PMIN = model(
     id="GEN",
     parameters=[
@@ -343,6 +360,41 @@ THERMAL_CLUSTER_MODEL_DHD = model(
             .shift(ExpressionRange(-param("d_min_down") + 1, literal(0)))
             .sum()
             <= param("nb_units_max").shift(-param("d_min_down")) - var("nb_on"),
+        ),
+    ],
+    objective_operational_contribution=(param("cost") * var("generation"))
+    .sum()
+    .expec(),
+)
+
+THERMAL_CLUSTER_MODEL_FAST = model(
+    id="GEN",
+    parameters=[
+        float_parameter("p_max", CONSTANT),  # p_max of a single unit
+        float_parameter("cost", CONSTANT),
+        int_parameter("nb_units_max", CONSTANT),
+        float_parameter("mingen", TIME_AND_SCENARIO_FREE),
+        float_parameter("failures", TIME_AND_SCENARIO_FREE),
+    ],
+    variables=[
+        float_variable(
+            "generation",
+            lower_bound=param("mingen"),
+            upper_bound=param("failures"),
+            structure=ANTICIPATIVE_TIME_VARYING,
+        ),
+    ],
+    ports=[ModelPort(port_type=BALANCE_PORT_TYPE, port_name="balance_port")],
+    port_fields_definitions=[
+        PortFieldDefinition(
+            port_field=PortFieldId("balance_port", "flow"),
+            definition=var("generation"),
+        )
+    ],
+    constraints=[
+        Constraint(
+            "Max generation",
+            var("generation") <= param("p_max") * param("nb_units_max"),
         ),
     ],
     objective_operational_contribution=(param("cost") * var("generation"))
