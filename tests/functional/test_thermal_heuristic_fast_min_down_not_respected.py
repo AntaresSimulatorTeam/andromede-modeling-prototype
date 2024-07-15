b6ec938912b03319c4535bcdd71bd1514283a8be
Juliette-Gerbaux <juliette.gerbaux_externe@rte-france.com>
2024-07-15 14:32:25 +0200

    New test

---
+++ tests/functional/test_thermal_heuristic_fast_min_down_not_respected.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2024, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+
+from pathlib import Path
+
+import numpy as np
+import ortools.linear_solver.pywraplp as pywraplp
+import pandas as pd
+import pytest
+
+from andromede.study import TimeScenarioSeriesData
+from andromede.study.data import ComponentParameterIndex
+from andromede.thermal_heuristic.problem import ThermalProblemBuilder
+from tests.functional.libs.lib_thermal_heuristic import THERMAL_CLUSTER_MODEL_MILP
+
+
+def test_fast_heuristic() -> None:
+    """
+    Solve the same problem as before with the heuristic fast of Antares
+    """
+    number_hours = 168
+    scenario = 0
+    week = 0
+
+    thermal_problem_builder = ThermalProblemBuilder(
+        number_hours=number_hours,
+        lp_relaxation=True,
+        fast=True,
+        data_dir=Path(__file__).parent
+        / "data/thermal_heuristic_fast_min_down_not_respected",
+        initial_thermal_model=THERMAL_CLUSTER_MODEL_MILP,
+        port_types=[],
+        models=[],
+        number_week=1,
+        list_scenario=list(range(1)),
+    )
+
+    cluster = "G1"
+
+    pmax = thermal_problem_builder.database.get_value(
+        ComponentParameterIndex(cluster, "p_max"), 0, 0
+    )
+    nb_on_1 = pd.DataFrame(
+        np.ceil(
+            np.round(
+                np.loadtxt(
+                    f"tests/functional/data/thermal_heuristic_fast_min_down_not_respected/itr1_fast_cluster.txt"
+                )  # type: ignore
+                / pmax,
+                12,
+            )
+        ),
+        index=list(range(week * number_hours, (week + 1) * number_hours)),
+        columns=[scenario],
+    )
+
+    thermal_problem_builder.database.add_data(
+        cluster, "n_guide", TimeScenarioSeriesData(nb_on_1)
+    )
+
+    # Solve heuristic problem
+    resolution_step_heuristic = (
+        thermal_problem_builder.get_resolution_step_fast_heuristic(
+            thermal_cluster=cluster,
+            week=week,
+            scenario=scenario,
+        )
+    )
+
+    status = resolution_step_heuristic.solve()
+    assert status == pywraplp.Solver.OPTIMAL
+    thermal_problem_builder.update_database_fast_after_heuristic(
+        resolution_step_heuristic.output, week, scenario, [cluster]
+    )
+
+    expected_output = np.loadtxt(
+        f"tests/functional/data/thermal_heuristic_fast_min_down_not_respected/itr2_fast_cluster.txt"
+    )
+    for t in range(number_hours):
+        assert thermal_problem_builder.database.get_value(
+            ComponentParameterIndex(cluster, "min_generating"), t, scenario
+        ) == pytest.approx(expected_output[t])
