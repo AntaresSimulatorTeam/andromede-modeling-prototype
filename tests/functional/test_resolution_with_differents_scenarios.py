1d0da2c21ed803c9f9bf500066218319b500477a
Juliette-Gerbaux <juliette.gerbaux_externe@rte-france.com>
2024-08-09 14:22:07 +0200

    Refactor

--- tests/functional/test_resolution_with_differents_scenarios.py
+++ tests/functional/test_resolution_with_differents_scenarios.py
@@ -10,7 +10,6 @@
 #
 # This file is part of the Antares project.
 
-from math import ceil, floor
 from typing import Dict, List
 
 import numpy as np
@@ -18,27 +17,13 @@ import ortools.linear_solver.pywraplp as pywraplp
 import pandas as pd
 import pytest
 
-from andromede.expression import literal, param, var
-from andromede.expression.expression import ExpressionRange, port_field
-from andromede.expression.indexing_structure import IndexingStructure
 from andromede.libs.standard import (
-    BALANCE_PORT_TYPE,
     DEMAND_MODEL,
     NODE_BALANCE_MODEL,
     SPILLAGE_MODEL,
     UNSUPPLIED_ENERGY_MODEL,
 )
-from andromede.model import Model, ModelPort, float_parameter, float_variable, model
-from andromede.model.constraint import Constraint
-from andromede.model.model import PortFieldDefinition, PortFieldId
-from andromede.model.parameter import float_parameter, int_parameter
-from andromede.model.variable import float_variable, int_variable
-from andromede.simulation import (
-    BlockBorderManagement,
-    OutputValues,
-    TimeBlock,
-    build_problem,
-)
+from andromede.simulation import BlockBorderManagement, TimeBlock, build_problem
 from andromede.simulation.optimization import OptimizationProblem
 from andromede.study import (
     ConstantData,
@@ -46,13 +31,11 @@ from andromede.study import (
     Network,
     Node,
     PortRef,
-    TimeIndex,
-    TimeScenarioIndex,
     TimeScenarioSeriesData,
-    TimeSeriesData,
     create_component,
 )
 from andromede.study.data import AbstractDataStructure
+from tests.functional.libs.lib_thermal_heuristic import THERMAL_CLUSTER_MODEL_MILP
 
 
 def test_one_problem_per_scenario() -> None:
@@ -71,8 +54,6 @@ def test_one_problem_per_scenario() -> None:
                         "G3": ConstantData(0),
                     },
                     number_hours,
-                    lp_relaxation=False,
-                    fast=False,
                     week=week,
                     scenarios=[scenario],
                 )
@@ -110,8 +91,6 @@ def test_one_problem_per_scenario_with_different_parameters() -> None:
                         "G3": ConstantData(0),
                     },
                     number_hours,
-                    lp_relaxation=False,
-                    fast=False,
                     week=week,
                     scenarios=[scenario],
                 )
@@ -146,8 +125,6 @@ def test_one_problem_for_all_scenarios() -> None:
             problem = create_complex_problem(
                 {"G1": ConstantData(0), "G2": ConstantData(0), "G3": ConstantData(0)},
                 number_hours,
-                lp_relaxation=False,
-                fast=False,
                 week=week,
                 scenarios=scenarios,
             )
@@ -180,8 +157,6 @@ def test_one_problem_for_all_scenarios_with_different_parameters() -> None:
             problem = create_complex_problem(
                 {"G1": ConstantData(0), "G2": ConstantData(0), "G3": ConstantData(0)},
                 number_hours,
-                lp_relaxation=False,
-                fast=False,
                 week=week,
                 scenarios=scenarios,
             )
@@ -205,101 +180,9 @@ def test_one_problem_for_all_scenarios_with_different_parameters() -> None:
             )
 
 
-CONSTANT = IndexingStructure(False, False)
-TIME_AND_SCENARIO_FREE = IndexingStructure(True, True)
-ANTICIPATIVE_TIME_VARYING = IndexingStructure(True, True)
-NON_ANTICIPATIVE_TIME_VARYING = IndexingStructure(True, False)
-CONSTANT_PER_SCENARIO = IndexingStructure(False, True)
-
-THERMAL_CLUSTER_MODEL_MILP = model(
-    id="GEN",
-    parameters=[
-        float_parameter("p_max", CONSTANT),  # p_max of a single unit
-        float_parameter("p_min", CONSTANT),
-        float_parameter("d_min_up", CONSTANT),
-        float_parameter("d_min_down", CONSTANT),
-        float_parameter("cost", CONSTANT),
-        float_parameter("startup_cost", CONSTANT),
-        float_parameter("fixed_cost", CONSTANT),
-        int_parameter("nb_units_min", TIME_AND_SCENARIO_FREE),
-        int_parameter("nb_units_max", CONSTANT),
-        float_parameter("failures", TIME_AND_SCENARIO_FREE),
-    ],
-    variables=[
-        float_variable(
-            "generation",
-            lower_bound=literal(0),
-            upper_bound=param("failures"),
-            structure=ANTICIPATIVE_TIME_VARYING,
-        ),
-        int_variable(
-            "nb_on",
-            lower_bound=param("nb_units_min"),
-            upper_bound=param("nb_units_max"),
-            structure=ANTICIPATIVE_TIME_VARYING,
-        ),
-        int_variable(
-            "nb_stop",
-            lower_bound=literal(0),
-            structure=ANTICIPATIVE_TIME_VARYING,
-        ),
-        int_variable(
-            "nb_start",
-            lower_bound=literal(0),
-            structure=ANTICIPATIVE_TIME_VARYING,
-        ),
-    ],
-    ports=[ModelPort(port_type=BALANCE_PORT_TYPE, port_name="balance_port")],
-    port_fields_definitions=[
-        PortFieldDefinition(
-            port_field=PortFieldId("balance_port", "flow"),
-            definition=var("generation"),
-        )
-    ],
-    constraints=[
-        Constraint(
-            "Max generation",
-            var("generation") <= param("p_max") * var("nb_on"),
-        ),
-        Constraint(
-            "Min generation",
-            var("generation") >= param("p_min") * var("nb_on"),
-        ),
-        Constraint(
-            "NODU balance",
-            var("nb_on") == var("nb_on").shift(-1) + var("nb_start") - var("nb_stop"),
-        ),
-        Constraint(
-            "Min up time",
-            var("nb_start")
-            .shift(ExpressionRange(-param("d_min_up") + 1, literal(0)))
-            .sum()
-            <= var("nb_on"),
-        ),
-        Constraint(
-            "Min down time",
-            var("nb_stop")
-            .shift(ExpressionRange(-param("d_min_down") + 1, literal(0)))
-            .sum()
-            <= param("nb_units_max").shift(-param("d_min_down")) - var("nb_on"),
-        ),
-        # It also works by writing ExpressionRange(-param("d_min_down") + 1, 0) as ExpressionRange's __post_init__ wraps integers to literal nodes. However, MyPy does not seem to infer that ExpressionRange's attributes are necessarily of ExpressionNode type and raises an error if the arguments in the constructor are integer (whereas it runs correctly), this why we specify it here with literal(0) instead of 0.
-    ],
-    objective_operational_contribution=(
-        param("cost") * var("generation")
-        + param("startup_cost") * var("nb_start")
-        + param("fixed_cost") * var("nb_on")
-    )
-    .sum()
-    .expec(),
-)
-
-
 def create_complex_problem(
     lower_bound: Dict[str, AbstractDataStructure],
     number_hours: int,
-    lp_relaxation: bool,
-    fast: bool,
     week: int,
     scenarios: List[int],
 ) -> OptimizationProblem:
