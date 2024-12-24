ace835071a2d3e2d78361ffe890c811dd35d764b
killian-scalian <killian.pirot@scalian.com>
2024-12-24 17:12:35 +0100

    lint

--- src/andromede/input_converter/src/utils.py
+++ src/andromede/input_converter/src/utils.py
@@ -68,5 +68,41 @@ def convert_renewable_to_component_list(
     return components
 
 
-def convert_hydro_to_component_list(areas: Area) -> list[InputComponent]:
+def convert_hydro_to_component_list(area: Area) -> list[InputComponent]:
+    raise NotImplementedError
+
+
+def convert_thermals_to_component_list(
+    areas: list[Area], root_path: Path
+) -> list[InputComponent]:
+    raise NotImplementedError
+
+
+def convert_load_matrix_to_component_list(
+    areas: list[Area], root_path: Path
+) -> list[InputComponent]:
+    raise NotImplementedError
+
+
+def convert_misc_gen_to_component_list(
+    areas: list[Area], root_path: Path
+) -> list[InputComponent]:
+    raise NotImplementedError
+
+
+def convert_reserves_matrix_to_component_list(
+    areas: list[Area], root_path: Path
+) -> list[InputComponent]:
+    raise NotImplementedError
+
+
+def convert_wind_matrix_to_component_list(
+    areas: list[Area], root_path: Path
+) -> list[InputComponent]:
+    raise NotImplementedError
+
+
+def convert_solar_matrix_to_component_list(
+    areas: list[Area], root_path: Path
+) -> list[InputComponent]:
     raise NotImplementedError
