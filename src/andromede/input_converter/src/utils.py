946a0cfaf597aff15c06e5a9392b53c65d468a92
killian-scalian <killian.pirot@scalian.com>
2025-01-08 17:59:24 +0100

    format black

--- src/andromede/input_converter/src/utils.py
+++ src/andromede/input_converter/src/utils.py
@@ -1,10 +1,7 @@
 from pathlib import Path
 
 import yaml
-from antares.craft.model.area import Area
 from pydantic import BaseModel
-
-from andromede.study.parsing import InputComponent, InputComponentParameter
 
 
 def resolve_path(path_str: Path) -> Path:
@@ -14,6 +11,7 @@ def resolve_path(path_str: Path) -> Path:
 
     absolute_path = path.resolve()
     return absolute_path
+
 
 def transform_to_yaml(model: BaseModel, output_path: str) -> None:
     with open(output_path, "w", encoding="utf-8") as yaml_file:
