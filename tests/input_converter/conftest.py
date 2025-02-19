3ec8150296f35d7b9003a83aeee34cf130a84ef3
killian-scalian <killian.pirot@scalian.com>
2025-02-19 14:39:14 +0100

    remove useless import

--- tests/input_converter/conftest.py
+++ tests/input_converter/conftest.py
@@ -9,8 +9,6 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # This file is part of the Antares project.
-import random
-
 import pandas as pd
 import pytest
 from antares.craft.model.area import Area, AreaPropertiesLocal
@@ -19,7 +17,6 @@ from antares.craft.model.renewable import (
 )
 from antares.craft.model.study import Study, create_study_local
 from antares.craft.tools.ini_tool import IniFile, InitializationFilesTypes
-from pathlib import Path
 
 
 @pytest.fixture
