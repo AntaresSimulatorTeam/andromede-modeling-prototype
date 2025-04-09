5a5f9a7ac9a6c37dc98fdf0dd4a6e9ccb99a1acd
OUSTRY Antoine <aoustry@gmail.com>
2025-04-09 10:48:40 +0200

    import format correction

--- tests/end-to-end/models/test_model_validation.py
+++ tests/end-to-end/models/test_model_validation.py
@@ -1,10 +1,24 @@
+# Copyright (c) 2024, RTE (https://www.rte-france.com)
+#
+# See AUTHORS.txt
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of the Antares project.
+
+import pytest
+
 from andromede.simulation.output_values import OutputValues
 from andromede.study.data import TimeIndex, TimeScenarioIndex
 from andromede.model.parsing import parse_yaml_library
 from andromede.model.resolve_library import resolve_library
 from andromede.study.resolve_components import resolve_system
-import pytest
-import pandas as pd
+
+
 
 @pytest.mark.parametrize(
     "system_file, optim_result_file, timespan, batch",
