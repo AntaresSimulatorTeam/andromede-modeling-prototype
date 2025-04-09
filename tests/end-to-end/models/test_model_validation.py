9e508ac9f476eb1a802c7e1d6a8c028c163da067
OUSTRY Antoine <aoustry@gmail.com>
2025-04-09 10:54:56 +0200

    import format correction

--- tests/end-to-end/models/test_model_validation.py
+++ tests/end-to-end/models/test_model_validation.py
@@ -12,8 +12,6 @@
 
 import pytest
 
-from andromede.simulation.output_values import OutputValues
-from andromede.study.data import TimeIndex, TimeScenarioIndex
 from andromede.model.parsing import parse_yaml_library
 from andromede.model.resolve_library import resolve_library
 from andromede.study.resolve_components import resolve_system
