0e0ca7aac2371092baa0b5b522e0846a36874c38
OUSTRY Antoine <aoustry@gmail.com>
2025-04-09 11:11:28 +0200

    import format correction

--- tests/end-to-end/models/test_model_validation.py
+++ tests/end-to-end/models/test_model_validation.py
@@ -10,17 +10,26 @@
 #
 # This file is part of the Antares project.
 
+from pathlib import Path
+
+import pandas as pd
 import pytest
 
 from andromede.model.parsing import parse_yaml_library
 from andromede.model.resolve_library import resolve_library
-from andromede.study.resolve_components import resolve_system
+from andromede.simulation.optimization import build_problem
+from andromede.simulation.time_block import TimeBlock
+from andromede.study.parsing import parse_yaml_components
+from andromede.study.resolve_components import (
+    build_data_base,
+    build_network,
+    resolve_system,
+)
 
 
 @pytest.mark.parametrize(
     "system_file, optim_result_file, timespan, batch",
-    [
-        ("dsr_validation.yml","dsr_case_results.csv",168,20),     
+    [   ("dsr_validation.yml","dsr_case_results.csv",168,20),     
         ("base_validation.yml","base_case_results.csv",168,20),
         ("electrolyser_validation.yml","electrolyser_case_results.csv",168,20),
         ("storage_validation.yml","storage_case_results.csv",168,20),
@@ -50,4 +59,8 @@ def test_model_behaviour(
         problem = build_problem(network, database, TimeBlock(1, [i for i in range(k*timespan,(k+1)*timespan)]), scenarios)
         status = problem.solver.Solve()
         assert status == problem.solver.OPTIMAL
-        assert(1e-6>abs(reference_values[k,0]-problem.solver.Objective().Value())/reference_values[k,0])
+        assert(
+            1e-6
+            > abs(reference_values[k,0]-problem.solver.Objective().Value())
+            / reference_values[k,0]
+        )
