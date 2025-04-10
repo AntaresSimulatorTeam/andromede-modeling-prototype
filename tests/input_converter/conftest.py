75074ec7e1d5e6c41f0ece51ed873c77873c8400
killian-scalian <killian.pirot@scalian.com>
2025-04-10 10:44:44 +0200

    isort

--- tests/input_converter/conftest.py
+++ tests/input_converter/conftest.py
@@ -13,8 +13,8 @@ import pandas as pd
 import pytest
 from antares.craft.model.area import Area, AreaProperties
 from antares.craft.model.hydro import HydroProperties
-from antares.craft.model.st_storage import STStorageProperties
 from antares.craft.model.renewable import RenewableClusterProperties
+from antares.craft.model.st_storage import STStorageProperties
 from antares.craft.model.study import Study, create_study_local
 from antares.craft.model.thermal import ThermalClusterProperties
 from antares.craft.tools.ini_tool import IniFile, InitializationFilesTypes
