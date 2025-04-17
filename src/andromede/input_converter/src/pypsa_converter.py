58ca526e72254e69bde5a3b88e5fd30e4a7ce07e
OUSTRY Antoine <aoustry@gmail.com>
2025-04-17 19:51:18 +0200

    Comment PyPSA converter

--- src/andromede/input_converter/src/pypsa_converter.py
+++ src/andromede/input_converter/src/pypsa_converter.py
@@ -47,11 +47,20 @@ class PyPSAStudyConverter:
     def __convert_pypsa_class(
         self,
         pypsa_df: DataFrame,
-        pypsa_dft: DataFrame,
+        pypsa_dft: dict[DataFrame],
         andromede_model: str,
         pypsa_params_to_andromede_params: dict,
         pypsa_params_to_andromede_connections: dict,
     ):
+        """
+        Generic function to handle the different PyPSA classes
+        pypsa_df: DataFrame : dataframe listing the components in the PyPSA class. Ex: pypsa_network.loads
+        pypsa_dft: dict[DataFrame] : dictionnary of dataframe, one for each parameter or variable that is time-varying for some compoentns. Ex: pypsa_network.loads_t
+        andromede_model: str : id of the model in the Andromede library
+        pypsa_params_to_andromede_params: dict : for each parameter of the PyPSA class that is to be exported in the Andromede model as a parameter, the name of the corresponding parameter in the Andromede model
+        pypsa_params_to_andromede_connections: dict, for each parameter of the PyPSA class that is to be exported in the Andromede model as a connection, a couple (model_port, bus_port)
+        """
+
         self.logger.info(f"Creating objects of type: {andromede_model}. ")
 
         # We test wether the keys of the conversion dictionnary given in input concern
@@ -67,10 +76,8 @@ class PyPSAStudyConverter:
         timedep_params = set(pypsa_params_to_andromede_params).intersection(
             pypsa_timedep
         )
-
+        # Save time series and memorize the time-dependant parameters
         timedep_comp_param = dict()
-
-        # Save time series and register
         for param in timedep_params:
             timedf = pypsa_dft[param]
             for component in timedf.columns:
@@ -81,22 +88,21 @@ class PyPSAStudyConverter:
                 )
 
         connections, components = [], []
-        if len(pypsa_params_to_andromede_connections) > 0:
-            for bus, model_port in pypsa_params_to_andromede_connections.items():
-                assert model_port != None
-                buses = pypsa_df[bus].values
-                for i, component in enumerate(pypsa_df.index):
-                    connections.append(
-                        InputPortConnections(
-                            component1=buses[i],
-                            port1="p_balance_port",
-                            component2=component,
-                            port2=model_port,
-                        )
+
+        for bus_id, couple in pypsa_params_to_andromede_connections.items():
+            model_port, bus_port = couple
+            buses = pypsa_df[bus_id].values
+            for i, component in enumerate(pypsa_df.index):
+                connections.append(
+                    InputPortConnections(
+                        component1=buses[i],
+                        port1=bus_port,
+                        component2=component,
+                        port2=model_port,
                     )
+                )
 
         for component in pypsa_df.index:
-
             components.append(
                 InputComponent(
                     id=component,
@@ -145,7 +151,7 @@ class PyPSAStudyConverter:
                 "sign": "sign",
                 "active": "active",
             },
-            {"bus": "p_balance_port"},
+            {"bus": ("p_balance_port", "p_balance_port")},
         )
 
     def __convert_pypsa_generatorsv0(self):
@@ -157,7 +163,7 @@ class PyPSAStudyConverter:
                 "p_nom": "p_nom",
                 "marginal_cost": "marginal_cost",
             },
-            {"bus": "p_balance_port"},
+            {"bus": ("p_balance_port", "p_balance_port")},
         )
 
     def __convert_pypsa_generators(self):
@@ -169,14 +175,14 @@ class PyPSAStudyConverter:
         print(
             "To be implemented, calling self.__convert_pypsa_class() with the right parameters"
         )
-        "The dictionnary pypsa_params_to_andromede_connections will have the form bus0: out_port, bus1:in_port... or the reverse!"
+        "The dictionnary pypsa_params_to_andromede_connections will have the form bus0: (p_out_port, p_balance_port), bus1:(p_in_port, p_balance_port)... or the reverse!"
 
     def __convert_pypsa_stores(self):
         print(
             "To be implemented, calling self.__convert_pypsa_class() with the right parameters"
         )
 
-    def __convert_pypsa_storage(self):
+    def __convert_pypsa_storages(self):
         print(
             "To be implemented, calling self.__convert_pypsa_class() with the right parameters"
         )
@@ -186,10 +192,15 @@ class PyPSAStudyConverter:
 
         self.logger.info("Study conversion started")
         list_components, list_connections = [], []
+
         methods = [
             self.__convert_pypsa_buses,
             self.__convert_pypsa_loads,
             self.__convert_pypsa_generatorsv0,
+            # self.__convert_pypsa_generators,
+            # self.__convert_pypsa_links,
+            # self.__convert_pypsa_stores,
+            # self.__convert_pypsa_storages,
         ]
 
         for method in methods:
